f39dc19b7f1ecc9687ae48eb6363215e1204db0b
sarahtarek97 <37047461+sarahtarek97@users.noreply.github.com>
2022-05-01 17:35:26 +0200

    error handling updates

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,57 @@
-module.exports = (err, req, res, next)=> {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err =>{
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err =>{
+  const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
+  const message = `Duplicate field value x, Please use another value!.`
+}
+
+const handleValidationErrorDB = err =>{
+
+}
+
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  })
+}
+
+const sendErrorForPro = (err, res)=>{ 
+  if(err.isOperational){
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     })
+  }else{
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very long!'
+    })
+  }
+  
+}
+
+module.exports = (err, req, res, next)=> {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+  
+    if(process.env.NODE_ENV === 'development'){
+      sendErrorForDev(err, res)
+    }else if(process.env.NODE_ENV === 'production'){
+      let error = {...err};
+
+      if(error.name === "CastError") error = handleCastErrorDB(error);
+      if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+      if(error.name === "ValidationError") error = handleValidationErrorDB(error);
+      
+
+      sendErrorForPro(error, res)
+    }
   }
--- server.js
+++ server.js
@@ -4,18 +4,29 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 mongoose.connect(DB,{
 useNewUrlParser: true
-}).then(con => {
+}).then(() => {
   console.log('DB connected!')
-}).catch(err=> {
-  console.log(err)
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err =>{
+  console.log(err.name, err.message);
+  server.close(()=> {
+    process.exit(1);
+  })
+})
+
+process.on('uncaughtException', err=> {
+  console.log(err.name, err.message);
+  server.close(()=> {
+    process.exit(1);
+  })
+})
